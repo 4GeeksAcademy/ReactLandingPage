175346a12850e001b8eff619c5ff6c590922952a
Alejandro Hipólito Hernando <126016967+Alejandro-Hipolito@users.noreply.github.com>
2023-05-05 08:01:03 +0000

    Adding home and footer

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+
+const Footer = () => {
+
+    return <footer style={{color: "white", fontSize: "1rem"}} className="footer pt-5"><div className="mt-3">Copyright © Your Website 2023</div></footer>
+}
+
+export default Footer; 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,23 +2,26 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Card from "./card.jsx"
+import Button from "./button.jsx";
+import Footer from "./footer.jsx";
 
 //create your first component
+
+
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="containter-fluid text-center">
+			<div className="row  mt-5 justify-content-center">
+					<Card image={"https://miro.medium.com/v2/resize:fit:800/1*8X19bupfY3zD6eXIv1UigQ.jpeg"} title={"Learn React"} text={"Aprender React te permite crear aplicaciones web modernas y dinámicas que pueden mejorar la experiencia del usuario."} url={"https://es.react.dev/"} type={"primary"}/>
+					<Card image={"https://oraclejet.org/wp-content/uploads/2022/07/Free-Courses-to-learn-JavaScript-1-800x500.jpg"} title={"Learn JavaScript"} text={"Aprender JavaScript te permite agregar interactividad y dinamismo a tus sitios web y aplicaciones web."} url={"https://developer.mozilla.org/es/docs/Web/JavaScript"} type={"warning"}/>
+					<Card image={"https://blog.sinapsis.agency/wp-content/uploads/2021/03/DEFINICIONES-42-HTML-800x500.jpg"} title={"Learn HTML"} text={"Aprender HTML te permite crear la estructura y el contenido de tus sitios web."} url={"https://developer.mozilla.org/es/docs/Web/HTML"}  type={"danger"}/>
+					<Card image={"https://i0.wp.com/fahmidasclassroom.com/wp-content/uploads/2020/02/css.jpg?fit=800%2C500&ssl=1"} title={"Learn CSS"} text={"Aprender CSS te permite dar estilo y diseño a tus sitios web."} url={"https://developer.mozilla.org/es/docs/Web/CSS"} type={"info"}/>
+			</div>
+			
+			<div className="footer mt-5 bg-dark" style={{height: "10rem"}}>
+				<Footer />
+			</div>
 		</div>
 	);
 };
